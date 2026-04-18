260a35282d7301fb32a2fd0e7b1cdcefef7b3b4b
Bismanjot06 <bismanbedi2006@gmail.com>
2026-04-18 13:16:54 +0530

    updated

--- client/components/app-sidebar.tsx
+++ client/components/app-sidebar.tsx
@@ -1,1 +1,2 @@
 export { AppSidebar } from "@/components/dashboard/admin/app-sidebar"
+
--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,14 @@
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
+const currentFilePath = fileURLToPath(import.meta.url)
+const currentDirPath = path.dirname(currentFilePath)
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	turbopack: {
+		root: currentDirPath,
+	},
+}
 
 export default nextConfig
